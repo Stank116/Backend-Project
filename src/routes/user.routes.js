719bf26f1006ca86d348f0f6d292e91b081d977a
Sumit Prasad <sumitprasad672@gmail.com>
2024-08-31 15:20:16 +0530

    adding more routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken} from "../controllers/user.controllers.js";
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, changeAccountDetails, changeUserAvatar, changeUserCoverImage} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { JWTverify } from "../middlewares/auth.middlewares.js";
 
@@ -21,5 +21,12 @@ userRouter.route("/register").post(
 userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(JWTverify, logoutUser)
 userRouter.route("refresh-token").post(refreshAccessToken)
+userRouter.route("/change-password").post(JWTverify, changeCurrentPassword )
+userRouter.route("/current-user").get(JWTverify, getCurrentUser )
+userRouter.route("/update-account").patch(JWTverify, changeAccountDetails )
+userRouter.route("/avatar").patch(JWTverify, upload.single("avatar") , changeUserAvatar)
+userRouter.route("/cover-image").patch(JWTverify, upload.single("coverImage") , changeUserCoverImage)
+
+
 
 export default userRouter
